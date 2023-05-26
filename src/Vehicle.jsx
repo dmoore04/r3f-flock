98e48d7a7cd51ca2ee4609fbdd05370ec754c298
dmoore04 <dmoore04@proton.me>
2023-05-26 20:31:56 +0100

    perf: reuse cone geometry and normal material

--- src/Vehicle.jsx
+++ src/Vehicle.jsx
@@ -1,20 +1,12 @@
-import { useEffect, useRef } from "react";
 import { useYuka } from "./useYuka";
 import { Vehicle } from "yuka";
+import * as THREE from "three";
+
+const coneGeometry = new THREE.ConeGeometry(0.1, 0.5, 8).rotateX(Math.PI / 2);
+const normalMaterial = new THREE.MeshNormalMaterial({ flatShading: true });
 
 export default function ConeVehicle(props) {
     const [ref] = useYuka({ type: Vehicle, ...props });
-    const geoRef = useRef();
 
-    useEffect(() => {
-        geoRef.current.rotateX(Math.PI / 2);
-        return () => geoRef.current.rotateX(-Math.PI / 2);
-    });
-
-    return (
-        <mesh ref={ref}>
-            <coneGeometry ref={geoRef} args={[0.1, 0.5, 8]} />
-            <meshNormalMaterial flatShading />
-        </mesh>
-    );
+    return <mesh ref={ref} geometry={coneGeometry} material={normalMaterial} />;
 }
