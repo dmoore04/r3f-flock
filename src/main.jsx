76a70b1ef719eb3d0b171ce84ede329251064c15
Danny Moore <56768881+dmoore04@users.noreply.github.com>
2023-06-03 19:10:51 +0000

    Update 8 files

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,28 @@ import Overlay from "./layout/Overlay.jsx";
 import useFlock from "./stores/useFlock.js";
 import WindowFocusHandler from "./layout/WindowFocusHandler.jsx";
 
+/**
+ * Thanks to 0xca0a for the great examples.
+ * Flying bananas sandbox (https://codesandbox.io/s/2ycs3?file=/src/index.js:29-35)
+ * made the overlay here super easy to implement.
+ *
+ * Yuka seek example (https://codesandbox.io/s/yuka-seek-demo-with-react-three-fiber-forked-tnv3q)
+ * also provided a great base for the flocking behavior.
+ *
+ * And of course thanks to Bruno Simon for Three.js Journey!
+ */
 function App() {
-    const flockCount = useFlock((state) => state.count);
+  const flockCount = useFlock((state) => state.count);
 
-    return (
-        <>
-            <Suspense fallback={null}>
-                <Flock count={flockCount} />
-            </Suspense>
-            <Overlay />
-            <WindowFocusHandler />
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={null}>
+        <Flock count={flockCount} />
+      </Suspense>
+      <Overlay />
+      <WindowFocusHandler />
+    </>
+  );
 }
 
 createRoot(document.getElementById("root")).render(<App />);
